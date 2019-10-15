4b9f977f24a4809424175373fcf318009eb9bb85
Caleb James DeLisle <cjd@cjdns.fr>
2019-10-15 15:07:24 +0200

    Added a few command line tools for manipulating difficulty

---
+++ cmd/compact2big/compact2big.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+
+	"github.com/pkt-cash/pktd/blockchain/packetcrypt/difficulty"
+)
+
+func usage() {
+	fmt.Print("Usage: compact2big <target>\n")
+}
+
+func main() {
+	if len(os.Args) < 2 {
+		usage()
+		return
+	}
+	arg := os.Args[1]
+	num, err := strconv.ParseUint(arg, 16, 32)
+	if err != nil {
+		fmt.Printf("Expected hex number, got [%s]\n", arg)
+		return
+	}
+	bigNum := difficulty.CompactToBig(uint32(num))
+	fmt.Printf("%s\n", bigNum.Text(16))
+}
---
+++ cmd/tar2work/tar2work.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"math/big"
+	"os"
+
+	"github.com/pkt-cash/pktd/blockchain/packetcrypt/difficulty"
+)
+
+func usage() {
+	fmt.Print("Usage: tar2work [-r] <target>")
+}
+
+func main() {
+	if len(os.Args) < 2 {
+		usage()
+		return
+	}
+	arg := os.Args[1]
+	reverse := false
+	if arg == "-r" {
+		if len(os.Args) < 3 {
+			usage()
+			return
+		}
+		arg = os.Args[1]
+	}
+	bigI := new(big.Int)
+	bigI.SetString(arg, 16)
+	var out *big.Int
+	if reverse {
+		out = difficulty.TargetForWork(bigI)
+	} else {
+		out = difficulty.WorkForTarget(bigI)
+	}
+	fmt.Printf("%s\n", out.Text(16))
+}
