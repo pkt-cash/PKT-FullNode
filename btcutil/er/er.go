a5d99f4f0cb03b13a1f7c86e5109fda41c554bc5
Caleb James DeLisle <cjd@cjdns.fr>
2019-11-04 00:26:12 +0100

    Added a much-needed error handler so that error events can be trapped, stack-traced, etc

---
+++ btcutil/er/er.go
@@ -0,0 +1,66 @@
+package er
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"runtime/debug"
+)
+
+type err struct {
+	e     error
+	stack []byte
+}
+
+type R interface {
+	Error() string
+	Wrapped() error
+	Is(error) bool
+}
+
+func (e err) Error() string {
+	if e.stack != nil {
+		fmt.Sprintf("%s\n%s", e.e.Error(), string(e.stack))
+	}
+	return e.e.Error()
+}
+
+func (e err) Wrapped() error {
+	return e.e
+}
+
+func (e err) SetWrapped(ee error) {
+	e.e = ee
+}
+
+func (e err) Is(er error) bool {
+	return e.e == er
+}
+
+func CaptureStack() []byte {
+	if "" == os.Getenv("ENABLE_STACKTRACE") {
+		return nil
+	}
+	return debug.Stack()
+}
+
+func New(s string) R {
+	return E(errors.New(s))
+}
+
+func Errorf(format string, a ...interface{}) R {
+	return E(fmt.Errorf(format, a...))
+}
+
+func E(e error) R {
+	if e == nil {
+		return nil
+	}
+	if e, ok := e.(err); ok {
+		return e
+	}
+	return err{
+		e:     e,
+		stack: CaptureStack(),
+	}
+}
