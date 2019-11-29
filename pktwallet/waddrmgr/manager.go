23d598ea95eaa30d48110642bdb450d0ad10942c
Caleb James DeLisle <cjd@cjdns.fr>
2019-11-29 10:50:32 +0100

    Wallet: Added networkStewardVote map which was missing from the wallet causing errors

--- pktwallet/waddrmgr/manager.go
+++ pktwallet/waddrmgr/manager.go
@@ -517,11 +517,12 @@ func (m *Manager) NewScopedKeyManager(ns walletdb.ReadWriteBucket, scope KeyScop
 	// Finally, we'll register this new scoped manager with the root
 	// manager.
 	m.scopedManagers[scope] = &ScopedKeyManager{
-		scope:       scope,
-		addrSchema:  addrSchema,
-		rootManager: m,
-		addrs:       make(map[addrKey]ManagedAddress),
-		acctInfo:    make(map[uint32]*accountInfo),
+		scope:              scope,
+		addrSchema:         addrSchema,
+		rootManager:        m,
+		addrs:              make(map[addrKey]ManagedAddress),
+		acctInfo:           make(map[uint32]*accountInfo),
+		networkStewardVote: make(map[uint32]*NetworkStewardVote),
 	}
 	m.externalAddrSchemas[addrSchema.ExternalAddrType] = append(
 		m.externalAddrSchemas[addrSchema.ExternalAddrType], scope,
@@ -1475,10 +1476,11 @@ func loadManager(ns walletdb.ReadBucket, pubPassphrase []byte,
 		}
 
 		scopedManagers[scope] = &ScopedKeyManager{
-			scope:      scope,
-			addrSchema: *scopeSchema,
-			addrs:      make(map[addrKey]ManagedAddress),
-			acctInfo:   make(map[uint32]*accountInfo),
+			scope:              scope,
+			addrSchema:         *scopeSchema,
+			addrs:              make(map[addrKey]ManagedAddress),
+			acctInfo:           make(map[uint32]*accountInfo),
+			networkStewardVote: make(map[uint32]*NetworkStewardVote),
 		}
 
 		return nil
