4388b1e1f19aa67840bfdea5c09a675871270209
Caleb James DeLisle <cjd@cjdns.fr>
2020-04-06 15:08:25 +0200

    Prepare to separate constants from wire

--- wire/msgtx.go
+++ wire/msgtx.go
@@ -18,38 +18,6 @@ import (
 )
 
 const (
-	// TxVersion is the current latest supported transaction version.
-	TxVersion = 1
-
-	// MaxTxInSequenceNum is the maximum sequence number the sequence field
-	// of a transaction input can be.
-	MaxTxInSequenceNum uint32 = 0xffffffff
-
-	// MaxPrevOutIndex is the maximum index the index field of a previous
-	// outpoint can be.
-	MaxPrevOutIndex uint32 = 0xffffffff
-
-	// SequenceLockTimeDisabled is a flag that if set on a transaction
-	// input's sequence number, the sequence number will not be interpreted
-	// as a relative locktime.
-	SequenceLockTimeDisabled = 1 << 31
-
-	// SequenceLockTimeIsSeconds is a flag that if set on a transaction
-	// input's sequence number, the relative locktime has units of 512
-	// seconds.
-	SequenceLockTimeIsSeconds = 1 << 22
-
-	// SequenceLockTimeMask is a mask that extracts the relative locktime
-	// when masked against the transaction input sequence number.
-	SequenceLockTimeMask = 0x0000ffff
-
-	// SequenceLockTimeGranularity is the defined time based granularity
-	// for seconds-based relative time locks. When converting from seconds
-	// to a sequence number, the value is right shifted by this amount,
-	// therefore the granularity of relative time locks in 512 or 2^9
-	// seconds. Enforced relative lock times are multiples of 512 seconds.
-	SequenceLockTimeGranularity = 9
-
 	// defaultTxInOutAlloc is the default size used for the backing array for
 	// transaction inputs and outputs.  The array will dynamically grow as needed,
 	// but this figure is intended to provide enough space for the number of
@@ -65,10 +33,6 @@ const (
 	// maxTxInPerMessage is the maximum number of transactions inputs that
 	// a transaction which fits into a message could possibly have.
 	maxTxInPerMessage = (MaxMessagePayload / minTxInPayload) + 1
-
-	// MinTxOutPayload is the minimum payload size for a transaction output.
-	// Value 8 bytes + Varint for PkScript length 1 byte.
-	MinTxOutPayload = 9
 
 	// maxTxOutPerMessage is the maximum number of transactions outputs that
 	// a transaction which fits into a message could possibly have.
