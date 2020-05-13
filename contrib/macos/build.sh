adb68c121ebc453f3c401fb06cfccb5124cc401e
Caleb James DeLisle <cjd@cjdns.fr>
2020-05-13 15:28:31 +0200

    Mac build needs to use prefix /usr/local otherwise it is not installable on new mac

--- contrib/macos/build.sh
+++ contrib/macos/build.sh
@@ -4,18 +4,4 @@
 # This script should be run from the project root
 # e.g. ./contrib/gem/build.sh
 #
-if which fpm; then
-	if which pkgbuild; then
-		fpm -n pktd-mac -s dir -t osxpkg -v "$(./bin/pktctl --version | sed 's/.* version //' | tr -d '\n')" ./bin
-		echo "PKG file built."
-	else
-		echo "pkgbuild not installed or not reachable"
-		exit 1
-	fi
-else
-	echo "fpm not installed or not reachable"
-	exit 1
-fi
-
-
-
+fpm --prefix /usr/local -n pktd-mac -s dir -t osxpkg -v "$(./bin/pktctl --version | sed 's/.* version //' | tr -d '\n')" ./bin
