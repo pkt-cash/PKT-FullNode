15d55bdcb324d1b894551ca1b243eaa2cfab1fdc
Thierry Marianne <thierry@marianne.io>
2021-06-25 17:42:18 +0200

    Ensure packages are built from the github workspace root

--- contrib/github-actions/build-linux-packages.sh
+++ contrib/github-actions/build-linux-packages.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
 function build() {
+  cd "${GITHUB_WORKSPACE}" || exit
   /bin/sh -c 'source ./do'
+
+  cd "${GITHUB_WORKSPACE}" || exit
   bash -x ./contrib/deb/build.sh
+
+  cd "${GITHUB_WORKSPACE}" || exit
   bash -x ./contrib/rpm/build.sh
 }
 build
--- contrib/github-actions/build-macos-package.sh
+++ contrib/github-actions/build-macos-package.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 function build() {
+  cd "${GITHUB_WORKSPACE}" || exit
   /bin/sh -c 'source ./do'
+
+  cd "${GITHUB_WORKSPACE}" || exit
   bash -x ./contrib/macos/build.sh
 }
 build
